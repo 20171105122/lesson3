066bcae2ca0223c600ba441b490cceedfda9907c
20171105122 <20171105122a@admindeiMac-38.local>
2018-09-27 09:53:00 +0800

    计算器
    有1，2，3等简单等按键，可进行加法运算，可归零

--- lesson3/ViewController.swift
+++ lesson3/ViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var temp = 0
+    
+    @IBOutlet weak var caculaterDispiay: UITextField!
+    
+    
+    @IBAction func button1(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"1"
+    }
+    
+    
+    @IBAction func button2(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"2"
+    }
+    
+    
+    @IBAction func button3(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"3"
+    }
+    
+    
+    @IBAction func buttonCaculator(_ sender: Any) {
+        var sum = 0
+        sum = temp + Int(caculaterDispiay.text!)!
+        caculaterDispiay.text = "\(sum)"
+    }
+    
+    
+    @IBAction func buttonAdd(_ sender: Any) {
+        temp = Int(caculaterDispiay.text!)!
+        caculaterDispiay.text = ""
+    }
+    
+    
+    @IBAction func clear(_ sender: Any) {
+        caculaterDispiay.text = ""
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
