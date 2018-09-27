e99d7398b9451e57bc177420f022ae53df27c7a8
20171105122 <20171105122a@admindeimac-38.local>
2018-09-27 09:20:25 +0800

    Initial Commit

---
+++ lesson3Tests/lesson3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lesson3Tests.swift
+//  lesson3Tests
+//
+//  Created by 冯佩玲 on 2018/9/27.
+//  Copyright © 2018年 冯佩玲. All rights reserved.
+//
+
+import XCTest
+@testable import lesson3
+
+class lesson3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
