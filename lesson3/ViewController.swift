56c15757430e0e25f0e63aaefb5861315ee8c1a2
20171105122 <20171105122a@admindeiMac-38.local>
2018-09-28 09:57:22 +0800

    新增计算器功能
    计算器运用条件语句增加了减法、乘法、除法和4～9数字按键，

--- lesson3/ViewController.swift
+++ lesson3/ViewController.swift
@@ -9,39 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var temp = 0
+    var temp = 0.00
+    var flag = 0
     
     @IBOutlet weak var caculaterDispiay: UITextField!
-    
     
     @IBAction func button1(_ sender: Any) {
         caculaterDispiay.text = caculaterDispiay.text!+"1"
     }
     
-    
     @IBAction func button2(_ sender: Any) {
         caculaterDispiay.text = caculaterDispiay.text!+"2"
     }
-    
     
     @IBAction func button3(_ sender: Any) {
         caculaterDispiay.text = caculaterDispiay.text!+"3"
     }
     
+    @IBAction func button4(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"4"
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"5"
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"6"
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"7"
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"8"
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+        caculaterDispiay.text = caculaterDispiay.text!+"9"
+    }
     
     @IBAction func buttonCaculator(_ sender: Any) {
-        var sum = 0
-        sum = temp + Int(caculaterDispiay.text!)!
-        caculaterDispiay.text = "\(sum)"
+        
+        if(flag==1){
+            var sum = 0.00
+            sum = temp + Double(caculaterDispiay.text!)!
+            caculaterDispiay.text = "\(sum)"
+        }
+        
+        if(flag==2){
+           var reduce = 0.00
+            reduce = temp - Double(caculaterDispiay.text!)!
+            caculaterDispiay.text = "\(reduce)"
+        }
+        
+        if(flag==3){
+            var multi = 0.00
+            multi = temp * Double(caculaterDispiay.text!)!
+            caculaterDispiay.text = "\(multi)"
+        }
+        
+        if(flag==4){
+            var dis = 0.00
+            dis = temp /  Double(caculaterDispiay.text!)!
+            caculaterDispiay.text = "\(dis)"
+        }
     }
-    
     
     @IBAction func buttonAdd(_ sender: Any) {
-        temp = Int(caculaterDispiay.text!)!
+        temp = Double(caculaterDispiay.text!)!
         caculaterDispiay.text = ""
+        flag = 1
     }
     
+    @IBAction func buttonsuntraction(_ sender: Any){
+        temp = Double(caculaterDispiay.text!)!
+        caculaterDispiay.text = ""
+        flag = 2
+    }
     
+    @IBAction func buttonmulti(_ sender: Any) {
+        temp = Double(caculaterDispiay.text!)!
+        caculaterDispiay.text = ""
+        flag = 3
+    }
+    @IBAction func buttondis(_ sender: Any) {
+        temp = Double(caculaterDispiay.text!)!
+        caculaterDispiay.text = ""
+        flag = 4
+    }
     @IBAction func clear(_ sender: Any) {
         caculaterDispiay.text = ""
     }
